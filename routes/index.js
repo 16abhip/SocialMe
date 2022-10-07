ee0e6f18ce1878b89c430c0fd49c4b46c8e269b9
16_abhip <abhip16singh@gmail.com>
2022-10-08 01:16:43 +0530

    Add comment to db and checking authentication including if post exits or not

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('Router loaded!');
 router.get('/',homeController.home );
 
 //  if any other routes use router.use('routername', req file);
-router.use('/post', require('./post'));
 router.use('/user', require('./user'));
+router.use('/post', require('./post'));
+router.use('/comment', require('./comment'));
 module.exports = router;

556a8338a0cae8024d938b4d1173267764a3bff1
16_abhip <abhip16singh@gmail.com>
2022-10-08 00:03:58 +0530

    Add post to DB and its controller and routes

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,3 +1,11 @@
-module.exports.add = function(req, res){
-    return res.end('<h1>AdD Post Here!</h1>');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    }, function(err, post){
+        if(err){console.log('Error in creating post'); return;}
+        return res.redirect('back');
+    });
 }
